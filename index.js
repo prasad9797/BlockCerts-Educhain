3fd8cbc95f6af8ed4074e714d367e40d8c17ca45
sujoydev99 <sujoydev99@gmail.com>
2020-08-13 20:20:17 +0530

    port update to 8080

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ app.use("/api/v1/public", Public);
 
 app.use(error);
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server is listening`));
+app.listen(process.env.PORT || 8080, () => console.log(`Server is listening`));
