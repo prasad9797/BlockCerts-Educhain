3f78318964d566501c9e5210a6da88341d40521a
Sujoy Dev <37466838+sujoydev99@users.noreply.github.com>
2020-12-19 15:10:51 +0530

    Update runner_manual.js

--- runner/runner_manual.js
+++ runner/runner_manual.js
@@ -1,3 +1,14 @@
+const pgp = require("../dbInit/dbConn").pgp;
+const getTransactionCount = require("../transaction-utils/certTransactions")
+  .getTransactionCount;
+const getRawTransaction = require("../transaction-utils/certTransactions")
+  .getRawTransaction;
+const signTransaction = require("../transaction-utils/certTransactions")
+  .signTransaction;
+const send = require("../transaction-utils/certTransactions").send;
+
+
+
 const runner = require("./runner.js");
 //const emailrunner = require("./emailrunner.js");
 runner();
